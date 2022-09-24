0a50e4d9f2b77298ecac8f203596ffcac1ffba6c
Flávio Dutra <flaviomdutra@outlook.com>
2022-09-23 22:07:13 -0300

    Created a new Expo app

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export function SignIn() {
+  return (
+    <View>
+      <Text>SignIn</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
