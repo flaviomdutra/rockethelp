4f49d3525e354e6d0af676d96e0d60150ae78076
Flávio Dutra <flaviomdutra@outlook.com>
2022-09-29 22:37:46 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,5 +1,22 @@
-import { SignIn } from "./src/screens/SignIn";
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+
+import { THEME } from './src/styles/theme';
+
+import { Loading } from './src/components/Loading';
+import { Details } from './src/screens/Details';
 
 export default function App() {
-  return <SignIn />;
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Details /> : <Loading />}
+    </NativeBaseProvider>
+  );
 }
